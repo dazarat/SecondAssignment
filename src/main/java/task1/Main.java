c197c104fd1685075dcd9289c90532ce121c0a47
Alex Glagolev <alexdead07@gmail.com>
2022-12-05 10:24:48 +0200

    fixed file paths from absolute to path from content root

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String INPUT = "C:\\Users\\dazarat\\Desktop\\ProfITSoft\\SecondHomework\\SecondAssignment\\src\\main\\resources\\input.xml";
-    public static final  String OUTPUT = "C:\\Users\\dazarat\\Desktop\\ProfITSoft\\SecondHomework\\SecondAssignment\\src\\main\\resources\\output.xml";
+    public static final String INPUT = "src/main/resources/input.xml";
+    public static final  String OUTPUT = "src/main/resources/output.xml";
 
     public static void main(String[] args) throws IOException {
 
--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -3,8 +3,8 @@ package task2;
 import java.io.IOException;
 
 public class Main {
-    public static final String INPUT = "C:\\Users\\dazarat\\Desktop\\ProfITSoft\\SecondHomework\\SecondAssignment\\src\\main\\resources\\fineFileDirectory";
-    public static final  String OUTPUT = "C:\\Users\\dazarat\\Desktop\\ProfITSoft\\SecondHomework\\SecondAssignment\\src\\main\\resources\\fineStatistic.xml";
+    public static final String INPUT = "src/main/resources/fineFileDirectory";
+    public static final  String OUTPUT = "src/main/resources/fineStatistic.xml";
 
     public static void main(String[] args) throws IOException {
         FinesProcessor.getFinesStatistics(INPUT, OUTPUT);
